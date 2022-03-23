b1b6505b0cb291240b8532d189cf1d0c2b39ba1e
fAndreuzzi <fandreuz@abel.maths.sissa.it>
2022-03-23 01:12:08 +0100

    minor fixes

--- dask/gm_sort.py
+++ dask/gm_sort.py
@@ -20,17 +20,18 @@ from gm import (
     fine_grid_spacing,
     solution1,
     solution2,
+    worker
 )
 
 # h is the uniform spacing
 def nonuniform_coord_to_uniform(pts, idx, h):
-    return np.floor(np.divide(pts[idx] + np.pi, h))
+   return np.floor(np.divide(pts[idx] + np.pi, h)).astype(int)
 
 
 # return a vector of bin coordinates ([0,1] means
 # the first bin of the second row)
-def which_bin(uniform_coord, bin_size):
-    return uniform_coord // bin_size
+def which_bin(uniform_coord, bin_dims):
+    return uniform_coord // bin_dims
 
 
 def bin_index(nbins_per_direction, bin_coords):
@@ -46,14 +47,14 @@ def bin_index(nbins_per_direction, bin_coords):
 # list of integers which can be used to re-arrange
 # a NumPy array) such that t(1),...,t(M1) belong to R1,
 # t(M1+1),...,t(M2) belong to R2 and so on
-def find_optimal_permutation(nbins, pts, h, bin_size):
+def find_optimal_permutation(nbins, pts, h, nbin_axes, bin_dims):
     bins = [[] for _ in range(nbins)]
     for j,pt in enumerate(pts):
-        uf_coords = nonuniform_coord_to_uniform(pts, h)
-        bin_coords = which_bin(uniform_coord, bin_size)
-        bins[bin_index(bin_size, bin_coords)].append(j)
+        uf_coords = nonuniform_coord_to_uniform(pts, j, h)
+        bin_coords = which_bin(uf_coords, bin_dims)
+        bins[bin_index(nbin_axes, bin_coords)].append(j)
     # flatten list
-    return functools.reduce(operator.iconcat, bins, [])
+    return reduce(operator.iconcat, bins, [])
 
 
 if __name__ == "__main__":
@@ -90,27 +91,30 @@ if __name__ == "__main__":
     vec_krn = np.vectorize(prt_kernel)
 
     n = fine_grid_size(N, w)
-    nbins = np.prod(np.ceil(np.divide(n, [m1, m2])))
+
+    bin_dims = np.array([m1, m2])
+    n_bins_axes = np.ceil(np.divide(n, bin_dims)).astype(int)
+    nbins = np.prod(n_bins_axes)
+
     h = fine_grid_spacing(n)
 
     alpha = compute_alpha(w, n)
 
-    t = find_optimal_permutation(nbins, pts, h, np.array([m1,m2]))
-    pts = pts[t]
-    f = f[t]
+    t = find_optimal_permutation(nbins, pts, h, n_bins_axes, bin_dims)
 
     remote_f = client.scatter(f)
     remote_pts = client.scatter(pts)
 
     start = time.time_ns()
+    # workers are spawned according to the new order
     b = np.sum(
         np.array(
             client.gather(
                 [
                     client.submit(
-                        worker, i, remote_pts, remote_f, vec_krn, h, alpha
+                        worker, j, remote_pts, remote_f, vec_krn, n, h, alpha
                     )
-                    for i in range(len(pts))
+                    for j in t
                 ]
             )
         ),
