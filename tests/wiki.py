3fdc4258e95f7138a9824696c85817748ae720b3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-04-05 23:34:25 +0200

    use my code for wiki data

--- tests/wiki.py
+++ tests/wiki.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mapped_distance_matrix import mapped_distance_matrix
 
 t = np.linspace(0, 2, 256)
 rng = np.random.default_rng(seed=2)
@@ -9,3 +10,12 @@ samples2 = np.stack((2 * rng.random(size=4), 2 * rng.random(size=4)), axis=-1)
 alpha = np.ones(samples2.shape[0])
 sigma = 1 / 12
 func = lambda x: np.exp(-x ** 2 / (2 * sigma ** 2))
+
+m = mapped_distance_matrix(samples1, samples2, 0.1, func)
+
+plt.contourf(x,y,m.dot(alpha).reshape(t.size, t.size), 50)
+plt.title('Sum of 4 (radial) Gaussians')
+plt.colorbar()
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.savefig('x.png')
